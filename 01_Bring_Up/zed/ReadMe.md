38a13583fca8347aac074ef7be9745f7d13d86c9
kaichu wu <kaichu.wu@neousys-tech.com>
2025-02-18 07:39:19 +0000

    [WIP] fine tuning Zed demo for NRU-170-PPC

---
+++ 01_Bring_Up/zed/ReadMe.md
@@ -0,0 +1,4 @@
+1. sudo cp zedx_ar0234.isp /var/nvidia/nvcam/settings
+2. sudo chmod 644 /var/nvidia/nvcam/settings/zedx_ar0234.isp
+3. reboot
+
---
+++ 02_Concurrent_Detection/build/CMakeFiles/ZED_concurrent_detections.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ZED_concurrent_detections.dir/src/GLViewer.cpp.o"
+  "CMakeFiles/ZED_concurrent_detections.dir/src/TrackingViewer.cpp.o"
+  "CMakeFiles/ZED_concurrent_detections.dir/src/main.cpp.o"
+  "ZED_concurrent_detections"
+  "ZED_concurrent_detections.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ZED_concurrent_detections.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
